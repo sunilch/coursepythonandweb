c0e6499ab8882f24315412d63f0177d4117bccc9
gs-schidara <schidara@gainsight.com>
2016-04-11 08:12:47 +0530

    adding wallpaper download codes

--- http/download_wallpaper_one.py
+++ http/download_wallpaper_one.py
@@ -25,29 +25,3 @@ for tag in wallpapertags:
     urllib.urlretrieve(linkname,dir+fname)
 
 #tag['src']
-
-'''
-def getextension(inpurl):
-    #returns the extension of the file name pointed to by inpurl
-    if inpurl==None:
-        return None
-    lastpart=inpurl.split('/')[-1]
-    if '.' not in lastpart:
-        return None
-    ext=lastpart.split('.')[-1]
-    return ext
-
-for tag in linktags:
-    myext=getextension(tag.get('href',None))
-    if myext not in allextensions and myext!=None:
-        allextensions.append(myext)
-
-print 'different extensions are '+','.join(allextensions)
-
-whichext=raw_input('Enter the type of extensions to be downloaded - ')
-basedir=raw_input('Enter the base directory name - ')
-
-exttags=soup.find_all('a',attrs={'href':re.compile('*\.'+whichext+'$')})
-
-print exttags
-'''
